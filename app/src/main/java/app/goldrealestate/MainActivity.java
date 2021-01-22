ebf1a26060c1fc4b849ab727f385f74d8b8518d1
joon34 <ukerrym@gmail.com>
2021-01-22 16:23:20 +0900

    MainActivity.java:2초 뒤 화면전환 Login: 위치조정

--- app/src/main/java/app/goldrealestate/MainActivity.java
+++ app/src/main/java/app/goldrealestate/MainActivity.java
@@ -1,8 +1,13 @@
 package app.goldrealestate;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+
+import app.goldrealestate.ui.login.LoginActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler hand = new Handler();
+
+        hand.postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    // TODO Auto-generated method stub
+                    Intent i = new Intent(MainActivity.this, LogIn.class);
+                    startActivity(i);
+                    finish();
+
+                }
+            }, 2000);
+
+
+        }
+
+
     }
-}
+
+
+
